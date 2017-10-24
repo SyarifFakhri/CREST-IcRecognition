7d455403cc4643f7d71ce27936af2287a2a56fb1
Syarif Fakhri <asybaf@gmail.com>
2017-10-24 22:21:12 +0800

    - added some files and cleaned up some code

--- IC-Recognition.py
+++ IC-Recognition.py
@@ -13,40 +13,47 @@ acceptedThreshold = 0.1
 "COLOUR TEMPLATE"
 cap = cv2.VideoCapture(0)
 #TODO - Sort according to size first
-#Begin Getting of Template
-imgTemplate = cv2.imread('component.jpg')
-imgTemplate = imutils.resize(imgTemplate, width=300)
 
-imgGrayTemplate = cv2.cvtColor(imgTemplate, cv2.COLOR_BGR2GRAY)          # get grayscale image
-imgBlurred = cv2.GaussianBlur(imgGrayTemplate, (5,5), 0)                        # blur
+def getTemplate():
+    # Begin Getting of Template
+    imgTemplate = cv2.imread('testTemplate9.png')
+    imgTemplate = imutils.resize(imgTemplate, width=300)
 
-# filter image from grayscale to black and white
-imgThresh = cv2.adaptiveThreshold(imgBlurred, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV, 11, 2)
-cv2.imshow('thres',imgTemplate)
+    imgGrayTemplate = cv2.cvtColor(imgTemplate, cv2.COLOR_BGR2GRAY)  # get grayscale image
+    imgBlurred = cv2.GaussianBlur(imgGrayTemplate, (5, 5), 0)  # blur
 
-imgThreshCopy = imgThresh.copy()
-imgContours, npaContours, npaHierarchy = cv2.findContours(imgThreshCopy, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-digits = {}
-template = {}
+    # filter image from grayscale to black and white
+    imgThresh = cv2.adaptiveThreshold(imgBlurred, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV, 11, 2)
+    cv2.imshow('thres', imgTemplate)
 
-for (i,c) in enumerate(npaContours):
-                                        # if contour is big enough to consider
-    [x, y, w, h] = cv2.boundingRect(c)
-    if cv2.contourArea(c) > MIN_CONTOUR_AREA:
-        roi = imgTemplate[y:y+h, x:x+w]
-        ###########
-        refgray = cv2.cvtColor(roi, cv2.COLOR_BGR2GRAY)
-        refblur = cv2.GaussianBlur(refgray, (1,1), 0)
-        refthresh = cv2.adaptiveThreshold(refblur, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV, 11, 2)
-        ###########
-        roi = cv2.resize(refthresh, (250, 100))
-        digits[i] = roi
+    imgThreshCopy = imgThresh.copy()
+    imgContours, npaContours, npaHierarchy = cv2.findContours(imgThreshCopy, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    digits = {}
+    template = {}
 
-#saving templates in sorted dictionary
-for (i,c) in enumerate(digits):
-    template[i] = digits[c]
-    cv2.imshow('template' + str(i), template[i])
-#getting image test and zoom the get the contour
+
+    for (i, c) in enumerate(npaContours):
+        # if contour is big enough to consider
+        [x, y, w, h] = cv2.boundingRect(c)
+        if cv2.contourArea(c) > MIN_CONTOUR_AREA:
+            roi = imgTemplate[y:y + h, x:x + w]
+            ###########
+            refgray = cv2.cvtColor(roi, cv2.COLOR_BGR2GRAY)
+            refblur = cv2.GaussianBlur(refgray, (1, 1), 0)
+            refthresh = cv2.adaptiveThreshold(refblur, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV, 11,
+                                              2)
+            ###########
+            roi = cv2.resize(refthresh, (250, 100))
+            digits[i] = roi
+
+    # saving templates in sorted dictionary
+    for (i, c) in enumerate(digits):
+        template[i] = digits[c]
+        cv2.imshow('template' + str(i), template[i])
+
+    return template
+
+template = getTemplate()
 
 arrayOfResults = []
 
@@ -231,6 +238,7 @@ while True:
 cap.release()
 #out.release()
 cv2.destroyAllWindows()
+
 
 '''
 cv2.waitKey(0)
